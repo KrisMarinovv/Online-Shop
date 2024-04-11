30cdf72e414d78392579ef3134f596c03c73d38e
Groppyy <165966903+KrisMarinovv@users.noreply.github.com>
2024-04-11 18:44:28 +0300

    Initial Commit

---
+++ Online Shop/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace Online_Shop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.roundControl5 = new Online_Shop.Resources.RoundControl();
+            this.roundControl4 = new Online_Shop.Resources.RoundControl();
+            this.roundControl3 = new Online_Shop.Resources.RoundControl();
+            this.roundControl2 = new Online_Shop.Resources.RoundControl();
+            this.roundControl1 = new Online_Shop.Resources.RoundControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.panel1.Controls.Add(this.roundControl5);
+            this.panel1.Controls.Add(this.roundControl4);
+            this.panel1.Controls.Add(this.roundControl3);
+            this.panel1.Controls.Add(this.roundControl2);
+            this.panel1.Controls.Add(this.roundControl1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 657);
+            this.panel1.TabIndex = 0;
+            // 
+            // roundControl5
+            // 
+            this.roundControl5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl5.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl5.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl5.BorderSize = 1;
+            this.roundControl5.ButtonBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl5.ButtonHighlightColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl5.ButtonHighlightColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl5.ButtonHighlightForeColor = System.Drawing.Color.White;
+            this.roundControl5.ButtonPressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl5.ButtonPressedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl5.ButtonPressedForeColor = System.Drawing.Color.White;
+            this.roundControl5.ButtonRoundRadius = 15;
+            this.roundControl5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.roundControl5.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.roundControl5.ForeColor = System.Drawing.Color.White;
+            this.roundControl5.Location = new System.Drawing.Point(12, 405);
+            this.roundControl5.Name = "roundControl5";
+            this.roundControl5.Size = new System.Drawing.Size(222, 57);
+            this.roundControl5.TabIndex = 5;
+            // 
+            // roundControl4
+            // 
+            this.roundControl4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl4.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl4.BorderSize = 1;
+            this.roundControl4.ButtonBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl4.ButtonHighlightColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl4.ButtonHighlightColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl4.ButtonHighlightForeColor = System.Drawing.Color.White;
+            this.roundControl4.ButtonPressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl4.ButtonPressedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl4.ButtonPressedForeColor = System.Drawing.Color.White;
+            this.roundControl4.ButtonRoundRadius = 15;
+            this.roundControl4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.roundControl4.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.roundControl4.ForeColor = System.Drawing.Color.White;
+            this.roundControl4.Location = new System.Drawing.Point(12, 342);
+            this.roundControl4.Name = "roundControl4";
+            this.roundControl4.Size = new System.Drawing.Size(222, 57);
+            this.roundControl4.TabIndex = 4;
+            // 
+            // roundControl3
+            // 
+            this.roundControl3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl3.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl3.BorderSize = 1;
+            this.roundControl3.ButtonBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl3.ButtonHighlightColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl3.ButtonHighlightColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl3.ButtonHighlightForeColor = System.Drawing.Color.White;
+            this.roundControl3.ButtonPressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl3.ButtonPressedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl3.ButtonPressedForeColor = System.Drawing.Color.White;
+            this.roundControl3.ButtonRoundRadius = 15;
+            this.roundControl3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.roundControl3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.roundControl3.ForeColor = System.Drawing.Color.White;
+            this.roundControl3.Location = new System.Drawing.Point(12, 279);
+            this.roundControl3.Name = "roundControl3";
+            this.roundControl3.Size = new System.Drawing.Size(222, 57);
+            this.roundControl3.TabIndex = 3;
+            // 
+            // roundControl2
+            // 
+            this.roundControl2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl2.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl2.BorderSize = 1;
+            this.roundControl2.ButtonBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl2.ButtonHighlightColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl2.ButtonHighlightColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl2.ButtonHighlightForeColor = System.Drawing.Color.White;
+            this.roundControl2.ButtonPressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl2.ButtonPressedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl2.ButtonPressedForeColor = System.Drawing.Color.White;
+            this.roundControl2.ButtonRoundRadius = 15;
+            this.roundControl2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.roundControl2.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.roundControl2.ForeColor = System.Drawing.Color.White;
+            this.roundControl2.Location = new System.Drawing.Point(12, 216);
+            this.roundControl2.Name = "roundControl2";
+            this.roundControl2.Size = new System.Drawing.Size(222, 57);
+            this.roundControl2.TabIndex = 2;
+            // 
+            // roundControl1
+            // 
+            this.roundControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl1.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl1.BorderSize = 1;
+            this.roundControl1.ButtonBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.roundControl1.ButtonHighlightColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl1.ButtonHighlightColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl1.ButtonHighlightForeColor = System.Drawing.Color.White;
+            this.roundControl1.ButtonPressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl1.ButtonPressedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(209)))), ((int)(((byte)(222)))));
+            this.roundControl1.ButtonPressedForeColor = System.Drawing.Color.White;
+            this.roundControl1.ButtonRoundRadius = 15;
+            this.roundControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.roundControl1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.roundControl1.ForeColor = System.Drawing.Color.White;
+            this.roundControl1.Location = new System.Drawing.Point(12, 153);
+            this.roundControl1.Name = "roundControl1";
+            this.roundControl1.Size = new System.Drawing.Size(222, 57);
+            this.roundControl1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(66, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Онлайн Магазин";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(162)))), ((int)(((byte)(227)))));
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(255, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(967, 54);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Online_Shop.Properties.Resources.UserImage;
+            this.pictureBox2.Location = new System.Drawing.Point(910, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Online_Shop.Properties.Resources.image;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(233)))));
+            this.ClientSize = new System.Drawing.Size(1222, 657);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Онлайн Магазин";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private Resources.RoundControl roundControl1;
+        private Resources.RoundControl roundControl5;
+        private Resources.RoundControl roundControl4;
+        private Resources.RoundControl roundControl3;
+        private Resources.RoundControl roundControl2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Online Shop/Resources/RoundedButton.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+
+namespace Online_Shop.Resources
+{
+    public partial class RoundControl : UserControl
+    {
+        private int m_BorderSize = 2;
+        private int m_ButtonRoundRadius = 15;
+
+        private bool IsHighlighted = false;
+        private bool IsPressed = false;
+
+        public RoundControl()
+        {
+            SetStyle(ControlStyles.Opaque |
+                     ControlStyles.AllPaintingInWmPaint |
+                     ControlStyles.ResizeRedraw |
+                     ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, false);
+            this.DoubleBuffered = false;
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            Size = new Size(100, 40);
+            BackColor = Color.FromArgb(187, 226, 236);
+            BackColor2 = Color.FromArgb(187, 226, 236);
+            ButtonBorderColor = Color.Black;
+            ButtonHighlightColor = Color.FromArgb(255, 232, 199);
+            ButtonHighlightColor2 = Color.FromArgb(255, 232, 199);
+            ButtonHighlightForeColor = Color.Black;
+
+            ButtonPressedColor = Color.FromArgb(255, 232, 199);
+            ButtonPressedColor2 = Color.FromArgb(255, 232, 199);
+            ButtonPressedForeColor = Color.White;
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x00000020;
+                return cp;
+            }
+        }
+
+        public int BorderSize
+        {
+            get => m_BorderSize;
+            set
+            {
+                m_BorderSize = Math.Max(Math.Min(value, 6), 0);
+                RepaintControl();
+            }
+        }
+
+        // Set Max = 44, Min = 1 to avoid quirks and exceptions
+        public int ButtonRoundRadius
+        {
+            get => m_ButtonRoundRadius;
+            set
+            {
+                m_ButtonRoundRadius = Math.Max(Math.Min(value, 44), 1);
+                RepaintControl();
+            }
+        }
+
+        public override string Text
+        {
+            get => base.Text;
+            set
+            {
+                base.Text = value;
+                RepaintControl();
+            }
+        }
+
+        // You should Invalidate the Parent also when these change
+        public Color BorderColor { get; set; } = Color.Tomato;
+        public Color BackColor2 { get; set; } = Color.Tomato;
+
+        public Color ButtonBorderColor { get; set; }
+        public Color ButtonHighlightColor { get; set; }
+        public Color ButtonHighlightColor2 { get; set; }
+        public Color ButtonHighlightForeColor { get; set; }
+        public Color ButtonPressedColor { get; set; }
+        public Color ButtonPressedColor2 { get; set; }
+        public Color ButtonPressedForeColor { get; set; }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.CompositingQuality = CompositingQuality.HighQuality;
+
+            var foreColor = IsPressed ? ButtonPressedForeColor : IsHighlighted ? ButtonHighlightForeColor : ForeColor;
+            var backColor = IsPressed ? ButtonPressedColor : IsHighlighted ? ButtonHighlightColor : BackColor;
+            var backColor2 = IsPressed ? ButtonPressedColor2 : IsHighlighted ? ButtonHighlightColor2 : BackColor2;
+            var rect = Path.GetBounds();
+
+            using (var pen = new Pen(ButtonBorderColor, m_BorderSize))
+            using (var pathBrush = new LinearGradientBrush(rect, backColor, backColor2, LinearGradientMode.Vertical))
+            using (var textBrush = new SolidBrush(foreColor))
+            using (var sf = new StringFormat())
+            {
+                sf.Alignment = StringAlignment.Center;
+                sf.LineAlignment = StringAlignment.Center;
+
+                e.Graphics.FillPath(pathBrush, Path);
+                if (m_BorderSize > 0) e.Graphics.DrawPath(pen, Path);
+
+                rect.Inflate(-4, -4);
+                e.Graphics.DrawString(Text, Font, textBrush, rect, sf);
+            }
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            IsHighlighted = true;
+            RepaintControl();
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            IsHighlighted = false;
+            IsPressed = false;
+            RepaintControl();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            IsPressed = true;
+            RepaintControl();
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            IsPressed = false;
+            RepaintControl();
+        }
+
+        private void RepaintControl()
+        {
+            Parent?.Invalidate(this.Bounds, true);
+            Invalidate();
+        }
+
+        private GraphicsPath Path
+        {
+            get
+            {
+                var rect = ClientRectangle;
+                int scaleOnBorder = -((m_BorderSize / 2) + 2);
+                rect.Inflate(scaleOnBorder, scaleOnBorder);
+                return GetRoundedRectangle(rect, m_ButtonRoundRadius);
+            }
+        }
+
+        private GraphicsPath GetRoundedRectangle(Rectangle rect, int d)
+        {
+            var gp = new GraphicsPath();
+            gp.StartFigure();
+            gp.AddArc(rect.X, rect.Y, d, d, 180, 90);
+            gp.AddArc(rect.X + rect.Width - d, rect.Y, d, d, 270, 90);
+            gp.AddArc(rect.X + rect.Width - d, rect.Y + rect.Height - d, d, d, 0, 90);
+            gp.AddArc(rect.X, rect.Y + rect.Height - d, d, d, 90, 90);
+            gp.CloseFigure();
+            return gp;
+        }
+    }
+}
